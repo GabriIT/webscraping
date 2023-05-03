a5709028127a05f673ea8afbfe30b1abd80fb581
GabriIT <vareca@live.com>
2023-05-03 14:22:56 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# Make a request
+page = requests.get(
+    "https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/")
+# Extract title of page
+
+soup = BeautifulSoup(page.content, 'html.parser')
+page_title = soup.title.text
+# Extract first <h1>(...)</h1> text
+first_h1 = soup.select('h1')[0].text
+second_h1 = soup.select('h1')[1].text
+first_h2 = soup.select('h2')[0].text
+print(page_title)
+"\n"
+print(first_h1)
+"\n"
+print(second_h1)
+"\n"
+print(first_h2)
